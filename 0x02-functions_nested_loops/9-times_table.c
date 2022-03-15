1574c1fa7626dd4d5f9531ee3f0d89da14edfef8
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-15 14:05:12 -0700

    prints the times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints the times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int repeat;
+	ehile (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			repeat = a * b;
+			if (b == 0)
+			{
+				_putchar('0' + rep);
+			}
+			else if (repeat < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + repeat);
+			}
+			else
+			{
+				_putchar('0' + repeat / 10);
+				_putchar('0' + repeat % 10);
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
