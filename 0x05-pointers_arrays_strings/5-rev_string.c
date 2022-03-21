dc03a37e3030f0f8661df19d8cdec800fd09623a
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-21 15:31:50 -0700

    reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,25 @@
+#include "main.h"
+/**rev_String - prints string in reverse
+ * @s: pointer
+ * Return: void
+ */
 
+void rev_string(char *s)
+{
+	int lgth;
+	int x;
+	int mid;
+	char tmp;
+
+	for (lgth = 0; s[lgth] != '\0'; lgth++)
+		x = 0;
+	mid = lgth / 2;
+
+	while (mid--)
+	{
+		tmp = s[lgth - x - 1];
+		s[lgth - x - 1] = s[x];
+		s[x] = tmp;
+		x++;
+	}
+}
