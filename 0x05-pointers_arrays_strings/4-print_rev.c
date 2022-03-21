7737b31bb26b3f1cbd3fd87bd81c4fd8e0996f3f
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-21 15:19:55 -0700

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: pointer
+ * Return:void
+ */
+
+void print_rev(char *s)
+{
+	int y = 0;
+
+	while (s[y])
+		y++;
+
+	wile (x--)
+	{
+		_putchar(s[y]);
+	}
+	_putchar('\n');
+}
