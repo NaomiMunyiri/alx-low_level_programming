2c851444f5f4b25c34af8c89d15cba7279d4ea79
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-21 15:50:50 -0700

    copies string pointed to it

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string pointed to it
+ * @dest: char
+ * @src: char
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = -1;
+
+	do
+	{
+		x++;
+		dest[x] = src[x];
+	}
+	while (src[x] != '\0');
+
+	return (dest);
+}
