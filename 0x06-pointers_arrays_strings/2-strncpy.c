e5583335eb99cf4e19e800ee069068d3aea07e7e
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-24 13:23:42 -0700

    concatenates two strings using at most n bytes from src

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - copy string
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, in n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
