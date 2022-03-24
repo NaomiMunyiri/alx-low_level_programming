a69f6322efe3b46cfd7650915a3787e5d903d414
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-24 13:02:41 -0700

    concatenates two strings using at most n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,23 @@
 #include"main.h"
+
 /**
- *_strncat - concatenates two strings using at most n bytes from src
+ * *_strncat - concatenates two strings using at most n bytes from src
  * @dest: pointer to destination string
  * @src: pointer to source string
  * @n: no of bytes to be concatenated
  * Return: pointer to destination
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int length, j;
 
-	length = 0;
-	
-	while (dest[length] != '\0')
-	{
-		length++;
-	}
-	for (j = 0; j < n && src[j] != '\0': j++, length++)
-	{
-		dest[length] = src[j];
-	}
-	dest[length] = '\0';
+	int x, i;
+
+	for (x = 0; dest[x] != '\0'; x++)
+
+		for (i = 0; i < n && src [i] != '\0'; i++)
+			dest[x + i] = src[i];
+
+	dest[x + i] = '\0';
 	return (dest);
 }
