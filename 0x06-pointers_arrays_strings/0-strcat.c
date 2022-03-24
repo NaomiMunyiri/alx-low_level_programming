2dda66b0c77354361ff59f2bfc6de6701b9491ca
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-24 12:46:46 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
+ *_strcat - concatenates two strings
  * @dest: where pointer is returned to
  * @src: source of pointer
  * Return: destination
