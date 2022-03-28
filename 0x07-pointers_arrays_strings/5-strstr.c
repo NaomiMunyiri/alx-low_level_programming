fad74220e886fc39dda8aaa2bec51384de79d127
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-28 16:08:49 -0700

    locates substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - loctaes substring
+ * @haystack: string
+ * @needle: string
+ * Return: pointer to beginning
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x;
+	int y;
+	int a = 0;
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		j = 0;
+		a = 1;
+		while (haystack[a] == needle[y] && needle[y] != '\0')
+		{
+			y++;
+			a++;
+		}
+		if (needle[y] == '\0')
+		{
+			return (haystack + x);
+		}
+	}
+	return (0);
+}
