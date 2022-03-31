2b503058f3d45184472acb16104eca83415cc609
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-31 06:16:12 -0700

    returns length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: pointer
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s > '\0')
+	{
+		a += _strlen_recursion(s + 1) + 1;
+	}
+	return (x);
+}
