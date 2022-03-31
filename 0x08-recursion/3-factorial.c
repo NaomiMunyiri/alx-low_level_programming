ccec5642103d89a92ce2bc024e8bb6b43316294a
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-03-31 06:20:25 -0700

    calc factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- *
+ * factorial - calc factorial
+ * @n: int to calc
+ * Return: n fact
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
