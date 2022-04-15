b740ab9ed54c9ae2801704dd423f309b96bbb78c
NaomiMunyiri <77190630+NaomiMunyiri@users.noreply.github.com>
2022-04-15 18:09:32 +0300

    Update 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,18 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints any type
+ * @format: arguments to print
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list list;
+int i = 0, j = 0;
+char *sep = "";
+
+}
+
+printf("\n");
